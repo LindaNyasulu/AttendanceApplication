d277293ebe73be0b26e8ed861930bf9527fe0a1e
LindaNyasulu <bsc-com-02-17@cc.ac.mw>
2021-08-14 01:35:50 -0600

    Added rolls for positioning the student

--- app/src/main/java/java/app/attendanceapplication/StudentActivity.java
+++ app/src/main/java/java/app/attendanceapplication/StudentActivity.java
@@ -2,12 +2,16 @@ package java.app.attendanceapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class StudentActivity extends AppCompatActivity {
@@ -16,7 +20,10 @@ public class StudentActivity extends AppCompatActivity {
     private String className;
     private String courseName;
     private int position;
-
+    private RecyclerView recyclerView;
+    private StudentAddapter addapter;
+    private  RecyclerView.LayoutManager layoutManager;
+    private ArrayList<StudentItem> studentItems  =  new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +35,12 @@ public class StudentActivity extends AppCompatActivity {
         position = intent.getIntExtra("position",-1);
 
         setToolbar();
+        recyclerView = findViewById(R.id.student_recycler);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        addapter=new StudentAddapter(this,studentItems);
+        recyclerView.setAdapter(addapter);
     }
 
     private void setToolbar() {
@@ -42,7 +55,27 @@ public class StudentActivity extends AppCompatActivity {
         coursetitle.setText(courseName);
 
         back.setOnClickListener(v->onBackPressed());
+        toolbar.inflateMenu(R.menu.student_menu);
+        toolbar.setOnMenuItemClickListener(menuItem->onMenuItemCick(menuItem));
 
+    }
+
+    private boolean onMenuItemCick(MenuItem menuItem) {
+        if (menuItem.getItemId()==R.id.add_student){
+            showAddStudentDialog();
+        }
+        return true;
+    }
+
+    private void showAddStudentDialog() {
+        MyDialog dialog = new MyDialog();
+        dialog.show(getSupportFragmentManager(),MyDialog.STUDENT_ADD_DIALOG);
+        dialog.setListener((roll,name)->addStudent(roll,name));
+    }
+
+    private void addStudent(String roll, String name) {
+        studentItems.add(new StudentItem(roll, name) );
+        addapter.notifyItemChanged(studentItems.size()-1);
     }
 
 }
